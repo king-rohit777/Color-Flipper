59c7f55551fcbb6a7adaeb0f58bd0caacc62b360
Rohit Jaiswal <62277327+king-rohit777@users.noreply.github.com>
2021-08-22 18:49:08 +0530

    Update hex.js

--- hex.js
+++ hex.js
@@ -4,7 +4,7 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
 btn.addEventListener("click", function() {
-
+      
     let hexColor = '#';
     for (var i = 0; i < 6; i++) {       
 
